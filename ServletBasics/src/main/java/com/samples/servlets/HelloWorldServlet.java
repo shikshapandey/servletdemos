e36f41c396fc73dd4787a7dff2ec641a565367ca
Pandey <shikpandey@teksystems.com>
2022-04-14 23:24:25 +0530

    helloworld

---
+++ ServletBasics/src/main/java/com/samples/servlets/HelloWorldServlet.java
@@ -0,0 +1,27 @@
+package com.samples.servlets;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.GenericServlet;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+
+public class HelloWorldServlet extends GenericServlet {
+
+	@Override
+	public void service(ServletRequest request, ServletResponse response) throws ServletException, IOException {
+		
+		System.out.println("Inside service Method of HelloWorldServlet....");
+		response.setContentType("text/html");
+		PrintWriter out = response.getWriter();
+		out.println("<html>");
+		out.println("<body>");
+		out.println("<h3>HelloWorld from Servlet!!</h3>");
+		out.println("</html>");
+		out.println("</body>");
+		
+	}
+
+}
